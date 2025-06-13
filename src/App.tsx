9ac5b8f74d110cb65f668e283cce2737b2e540f9
mapecha <m.peckyn@gmail.com>
2025-06-13 16:26:04 +0200

    feat: Remove testing banner from top of app

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,6 @@ import { products } from './data/products';
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Testing Banner */}
-      <div className="bg-yellow-400 border-b-2 border-yellow-600 px-4 py-3">
-        <div className="flex items-center justify-center">
-          <div className="text-center">
-            <span className="text-black font-bold text-lg">🧪 TESTING ENVIRONMENT</span>
-            <p className="text-black text-sm mt-1">
-              This is a testing version of Product Pantry - Changes are being tested before going live
-            </p>
-          </div>
-        </div>
-      </div>
-      
       <ProductTable products={products} />
     </div>
   );
